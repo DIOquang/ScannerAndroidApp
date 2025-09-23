e824b266b07c2684d9cde557bf54f17c36a3c342
pmquang <minhquangkzd03@gmail.com>
2025-09-23 18:37:28 +0700

    First commit

---
+++ app/src/main/java/com/example/scannerapp/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.scannerapp;
+
+import android.os.Bundle;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        // Gán layout cho activity
+        setContentView(R.layout.activity_main);
+    }
+}
---
+++ app/src/main/java/com/example/scannerapp/ScanFragment.java
@@ -0,0 +1,4 @@
+package com.example.scannerapp;
+
+public class ScanFragment {
+}
