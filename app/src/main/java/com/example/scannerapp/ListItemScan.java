c58359cab8dd510416efa173d18baa66b1d4af99
pmquang <minhquangkzd03@gmail.com>
2025-09-24 06:50:49 +0700

    second

--- app/src/main/java/com/example/scannerapp/ListItemScan.java
+++ app/src/main/java/com/example/scannerapp/ListItemScan.java
@@ -8,12 +8,12 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
-public class ConvertFragment extends Fragment {
+public class ListItemScan extends Fragment {
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater,
                              @Nullable ViewGroup container,
                              @Nullable Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_convert, container, false);
+        return inflater.inflate(R.layout.list_item_scan, container, false);
     }
 }
--- app/src/main/java/com/example/scannerapp/MainActivity.java
+++ app/src/main/java/com/example/scannerapp/MainActivity.java
@@ -1,13 +1,46 @@
 package com.example.scannerapp;
 
 import android.os.Bundle;
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+
+import com.google.android.material.bottomnavigation.BottomNavigationView;
 
 public class MainActivity extends AppCompatActivity {
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        // Gán layout cho activity
         setContentView(R.layout.activity_main);
+
+        BottomNavigationView bottomNav = findViewById(R.id.bottom_navigation);
+
+        // mặc định mở ScanFragment
+        if (savedInstanceState == null) {
+            getSupportFragmentManager().beginTransaction()
+                    .replace(R.id.fragment_container, new ScanFragment())
+                    .commit();
+        }
+
+        bottomNav.setOnItemSelectedListener(item -> {
+            Fragment selectedFragment = null;
+            int id = item.getItemId();
+
+            if (id == R.id.nav_scan) {
+                selectedFragment = new ScanFragment();
+            } else if (id == R.id.nav_convert) {
+                selectedFragment = new ConvertFragment();
+            } else if (id == R.id.nav_form) {
+                selectedFragment = new FormFragment();
+            }
+
+            if (selectedFragment != null) {
+                getSupportFragmentManager().beginTransaction()
+                        .replace(R.id.fragment_container, selectedFragment)
+                        .commit();
+            }
+            return true;
+        });
     }
 }
